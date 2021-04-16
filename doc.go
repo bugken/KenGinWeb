a4666cd67a577e5e66f0c43f03e8e29c6c5f69d7
ken yan <kenyan1013@gmail.com>
2021-04-16 21:39:18 +0800

    增加doc文件

---
+++ doc.go
@@ -0,0 +1,18 @@
+package NetClassGinWeb
+
+/*
+type Engine struct {
+//
+}
+var _ IRouter = &Engine{}
+一个结构体下面会写如上一句话，是为了确保结构体实现了定义的接口，把问题暴露在编译阶段。这种写
+	法在第三方库种很常见，也算是_的一种妙用。
+*/
+
+/*
+gin.Context方法:
+	c.Set()
+	c.Get()
+	c.Abort()
+	c.Next()
+*/
